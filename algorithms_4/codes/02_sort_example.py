fea9ed70c2dd4fd629fc0a95a9865e1f7fc7f7e9
Louis <azdxyx@163.com>
2020-10-21 00:00:11 +0800

    sort part 1

--- algorithms_4/codes/02_sort_example.py
+++ algorithms_4/codes/02_sort_example.py
@@ -29,9 +29,61 @@ class SelectionSort(Example):
                 if self.less(self.elems[j], self.elems[index]):                   
                     index = j
             self.exchange(i, index)
+            
 
+class InsertSort_my(Example):
+    def sort(self):
+        for i in range(1, len(self.elems)):
+            value = self.elems[i]
+            for j in range(i+1):
+                if self.less(self.elems[i], self.elems[j]):                   
+                    for k in range(i, j, -1):
+                        self.elems[k] = self.elems[k-1]
+                    break
+            self.elems[j] = value
+            
+            
+class InsertSort(Example):
+    def sort(self):
+        for i in range(1, len(self.elems)):
+            j = i
+            while j > 0 and self.less(self.elems[j], self.elems[j-1]):
+                self.exchange(j, j-1)
+                j -= 1
+                    
+                    
+class InsertSort2(Example):
+    def sort(self):
+        for i in range(1, len(self.elems)):
+            j = i-1
+            value = self.elems[i]
+            while j >= 0 and self.less(value, self.elems[j]):
+                self.elems[j+1] = self.elems[j]
+                j -= 1
+            self.elems[j+1] = value
+
+
+    
 
 if __name__ == "__main__":
-    e = SelectionSort([5,4,3,2,1])
-    e.sort()
-    print(e.show())
+    # e = SelectionSort([5,4,3,2,1])
+    # e = InsertSort([5,4,3,2,1,10,66,33,22,66])
+    # e = InsertSort_my([5,4,3,2,1,10,66,33,22,66])
+    # e = InsertSort2([5,4,3,2,1,10,66,33,22,66])
+    # e.sort()
+    # print(e.show())
+    
+    import timeit
+    arr = [5,4,3,2,1,10,66,33,22]
+    ssort = SelectionSort(arr)
+    isort = InsertSort(arr)
+    isort_my = InsertSort_my(arr)
+    isort2 = InsertSort2(arr)
+    t1 = (timeit.timeit(stmt=ssort.sort, number=100000))
+    t2 = (timeit.timeit(stmt=isort.sort, number=100000))
+    t3 = (timeit.timeit(stmt=isort_my.sort, number=100000))
+    t4 = (timeit.timeit(stmt=isort2.sort, number=100000))
+    print(t1) # 1.75
+    print(t2) # 0.32
+    print(t3) # 1.5
+    print(t4) # 0.41
